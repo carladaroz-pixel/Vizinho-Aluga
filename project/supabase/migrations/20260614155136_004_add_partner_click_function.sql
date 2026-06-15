34eadc1fdf1cbe4cce295194c204a2855db5a3a7
carladaroz-pixel <carla.daroz@gmail.com>
2026-06-15 20:30:15 -0300

    Delete project directory

--- project/supabase/migrations/20260614155136_004_add_partner_click_function.sql
+++
@@ -1,9 +0,0 @@
--- Function to increment partner clicks
-CREATE OR REPLACE FUNCTION increment_partner_clicks(partner_id uuid)
-RETURNS void AS $$
-BEGIN
-  UPDATE services_partners
-  SET clicks = clicks + 1
-  WHERE id = partner_id;
-END;
-$$ LANGUAGE plpgsql SECURITY DEFINER;
